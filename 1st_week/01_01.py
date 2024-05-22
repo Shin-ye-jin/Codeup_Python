0c8858e9793d6f9b2b77436aeda05f167b79be9c
S510U <fx0605@naver.com>
2024-05-22 18:58:49 +0900

    //0522

--- 1st_week/01_01.py
+++ 1st_week/01_01.py
@@ -1,16 +1,28 @@
-#1101
-print("Hello, World!")
+# #1101
+# print("Hello, World!")
+#
+# #1102
+# print("Hello,\nWorld!")
+#
+# #1103
+# print("\"c:\\test\"")
+#
+# #1731
+# print("special charaters")
+# print("[\\n,\\\",\\\\] is very important.")
+#
+# #1110
+# a=input()
+# print(a)
 
-#1102
-print("Hello,\nWorld!")
+# # 1111
+# a=input()
+# print(a+"%")
 
-#1103
-print("\"c:\\test\"")
+# # 1112
+# a,b=input().split()
+# print(a,b)
 
-#1731
-print("special charaters")
-print("[\\n,\\\",\\\\] is very important.")
-
-#1110
-a=input()
-print(a)
+# 1113
+a,b=input().split()
+print(b,a)
