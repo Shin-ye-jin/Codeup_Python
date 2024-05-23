260e3f7f046013fbd30fbfde0ab1cfce099c704f
S510U <fx0605@naver.com>
2024-05-23 18:42:52 +0900

    //0523

--- 1st_week/01_01.py
+++ 1st_week/01_01.py
@@ -24,5 +24,26 @@
 # print(a,b)
 
 # 1113
+# a,b=input().split()
+# print(b,a)
+
+# 1114
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# print(a+b)
+
+# 1115
+# a,b=input().split()
+# a = int(a)
+# b = int(b)
+# print(a+b)
+
+# 1116 (확인~)
 a,b=input().split()
-print(b,a)
+a=int(a)
+b=int(b)
+print("%d+%d=%d" % (a,b,int(a+b)))
+print("%d-%d=%d" % (a,b,int(a-b)))
+print("%d*%d=%d" % (a,b,int(a*b)))
+print("%d/%d=%d" % (a,b,int(a/b)))
