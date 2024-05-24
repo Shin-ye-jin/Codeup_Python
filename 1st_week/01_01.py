081a0aa28f7eb08b9987be9292a492c9a282ac16
S510U <fx0605@naver.com>
2024-05-25 01:07:49 +0900

    //0523

--- 1st_week/01_01.py
+++ 1st_week/01_01.py
@@ -40,10 +40,26 @@
 # print(a+b)
 
 # 1116 (확인~)
-a,b=input().split()
-a=int(a)
-b=int(b)
-print("%d+%d=%d" % (a,b,int(a+b)))
-print("%d-%d=%d" % (a,b,int(a-b)))
-print("%d*%d=%d" % (a,b,int(a*b)))
-print("%d/%d=%d" % (a,b,int(a/b)))
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# print("%d+%d=%d" % (a,b,int(a+b)))
+# print("%d-%d=%d" % (a,b,int(a-b)))
+# print("%d*%d=%d" % (a,b,int(a*b)))
+# print("%d/%d=%d" % (a,b,int(a/b)))
+
+# 1117
+# a,b=input().split()
+# a=float(a)
+# b=float(b)
+# print(round(a*b,2))
+
+# 1118
+# a,b=input().split()
+# a=float(a)
+# b=float(b)
+# print(round(a*b/2,1))
+
+# 1119
+a=int(input())
+print(a*24)
