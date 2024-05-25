7f2e740e0c71a65e6ae7912a66ce64484cd070ed
S510U <fx0605@naver.com>
2024-05-26 00:42:39 +0900

    //0526

--- 1st_week/01_01.py
+++ 1st_week/01_01.py
@@ -65,4 +65,30 @@
 # print(a*24)
 
 # 1120
+# a,b,c=input().split()
+# a=float(a)
+# b=float(b)
+# c=float(c)
+# avg = (a+b+c)/3
+# print("%.2f" % avg)
 
+# 1121
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# print(a%b)
+
+# 1122
+# a = int(input())
+# m = a//60
+# s = a%60
+# print(m,s)
+
+# 1123
+# a=int(input())
+# f = 9/5*a+32
+# print("%.3f" % f)
+
+# 1125
+# a = int(input())
+# print("%o"%a,"%X"%a)
