3ce7ff4be743f3b32c912009ebfdf20f88c70b0f
S510U <fx0605@naver.com>
2024-05-27 18:48:32 +0900

    //0527

---
+++ 1st_week/02_01.py
@@ -0,0 +1,38 @@
+# 1131
+# a = input()
+# print(a)
+
+# 1132
+# a=input()
+# print(a)
+
+# 1133
+# a=input()
+# print(a)
+
+# 1135
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# if a>=b:
+#     print(1)
+# else:
+#     print(0)
+
+# 1136
+# a,b= input().split()
+# a=int(a)
+# b=int(b)
+# if a==b:
+#     print(1)
+# else:
+#     print(0)
+
+# 1137
+a,b=input().split()
+a=int(a)
+b=int(b)
+if a!=b:
+    print(1)
+else:
+    print(0)
