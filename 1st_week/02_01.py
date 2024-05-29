a68cad763c1988b3ee6828aca36b6b0dfc071125
S510U <fx0605@naver.com>
2024-05-29 11:52:11 +0900

    //0529

--- 1st_week/02_01.py
+++ 1st_week/02_01.py
@@ -29,10 +29,26 @@
 #     print(0)
 
 # 1137
-a,b=input().split()
-a=int(a)
-b=int(b)
-if a!=b:
-    print(1)
-else:
-    print(0)
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# if a!=b:
+#     print(1)
+# else:
+#     print(0)
+
+# 1138
+# a=int(input())
+# print(int(not a)) # 정수형으로 변환
+
+# 1139
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# print(a and b)
+
+# 1140
+# a,b = input().split()
+# a=int(a)
+# b=int(b)
+# print(a or b)
