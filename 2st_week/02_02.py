b0da1974a27a6fadde0a2c4f4d9702c9526f2cbd
S510U <fx0605@naver.com>
2024-05-30 22:35:00 +0900

    //0530

--- 2st_week/02_02.py
+++ 2st_week/02_02.py
@@ -10,3 +10,44 @@
 # b=int(b)
 # print(a|b)
 
+# 1147
+# a,b = input().split()
+# a=int(a)
+# b=int(b)
+# print(a << b)
+
+# 1148
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# print(a>>b)
+
+# 1149
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+
+# 1150
+a,b,c=input().split()
+a=int(a)
+b=int(b)
+c=int(c)
+if a>b:
+    if b>c :
+        print(c)
+    else:
+        print(b)
+elif a>c:
+    if c>b:
+        print(b)
+    else:
+        print(c)
+else:
+    if c>a:
+        print(a)
+    else:
+        print(c)
