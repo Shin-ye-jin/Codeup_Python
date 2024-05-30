efad038b0fdb58c01820ea5004ce160cf15b73ad
S510U <fx0605@naver.com>
2024-05-30 22:40:12 +0900

    //0530 주석

--- 1st_week/02_01.py
+++ 1st_week/02_01.py
@@ -1,16 +1,16 @@
-# 1131
+# 1131 문자 출력하기
 # a = input()
 # print(a)
 
-# 1132
+# 1132 문자열 출력하기
 # a=input()
 # print(a)
 
-# 1133
+# 1133 공백이 있는 문자열 입출력
 # a=input()
 # print(a)
 
-# 1135
+# 1135 관계 연산자1
 # a,b=input().split()
 # a=int(a)
 # b=int(b)
@@ -19,7 +19,7 @@
 # else:
 #     print(0)
 
-# 1136
+# 1136 관계 연산자2
 # a,b= input().split()
 # a=int(a)
 # b=int(b)
@@ -28,7 +28,7 @@
 # else:
 #     print(0)
 
-# 1137
+# 1137 관계 연산자3
 # a,b=input().split()
 # a=int(a)
 # b=int(b)
@@ -37,17 +37,17 @@
 # else:
 #     print(0)
 
-# 1138
+# 1138 논리 연산자(NOT)
 # a=int(input())
 # print(int(not a)) # 정수형으로 변환
 
-# 1139
+# 1139 논리 연산자(AND)
 # a,b=input().split()
 # a=int(a)
 # b=int(b)
 # print(a and b)
 
-# 1140
+# 1140 논리 연산자(OR)
 # a,b = input().split()
 # a=int(a)
 # b=int(b)
