a3715edbbca8a9684d5d52c3929d9c025e373b0a
S510U <fx0605@naver.com>
2024-05-30 22:43:03 +0900

    //0530 주석

--- 1st_week/01_01.py
+++ 1st_week/01_01.py
@@ -1,45 +1,45 @@
-# #1101
+# #1101 Hello, Wolrd!
 # print("Hello, World!")
 #
-# #1102
+# #1102 Hello, World! (줄 바꿈 버전)
 # print("Hello,\nWorld!")
 #
-# #1103
+# #1103 폴더명 출력
 # print("\"c:\\test\"")
 #
-# #1731
+# #1731 특수 서식 문자 출력하기
 # print("special charaters")
 # print("[\\n,\\\",\\\\] is very important.")
 #
-# #1110
+# #1110 정수 그대로 출력하기
 # a=input()
 # print(a)
 
-# # 1111
+# # 1111 % 출력
 # a=input()
 # print(a+"%")
 
-# # 1112
+# # 1112 두 정수 출력
 # a,b=input().split()
 # print(a,b)
 
-# 1113
+# 1113 바꿔서 출력하기
 # a,b=input().split()
 # print(b,a)
 
-# 1114
+# 1114 두 정수의 덧셈
 # a,b=input().split()
 # a=int(a)
 # b=int(b)
 # print(a+b)
 
-# 1115
+# 1115두 정수의 덧셈(64비트)
 # a,b=input().split()
 # a = int(a)
 # b = int(b)
 # print(a+b)
 
-# 1116 (확인~)
+# 1116 (확인~) 사칙연산 계산기
 # a,b=input().split()
 # a=int(a)
 # b=int(b)
@@ -48,23 +48,23 @@
 # print("%d*%d=%d" % (a,b,int(a*b)))
 # print("%d/%d=%d" % (a,b,int(a/b)))
 
-# 1117
+# 1117 두 실수의 곱
 # a,b=input().split()
 # a=float(a)
 # b=float(b)
 # print(round(a*b,2))
 
-# 1118
+# 1118 삼각형의 넓이 구하기
 # a,b=input().split()
 # a=float(a)
 # b=float(b)
 # print(round(a*b/2,1))
 
-# 1119
+# 1119 일을 시간으로 변환
 # a=int(input())
 # print(a*24)
 
-# 1120
+# 1120 세 수의 평균
 # a,b,c=input().split()
 # a=float(a)
 # b=float(b)
@@ -72,19 +72,19 @@
 # avg = (a+b+c)/3
 # print("%.2f" % avg)
 
-# 1121
+# 1121 나머지 구하기
 # a,b=input().split()
 # a=int(a)
 # b=int(b)
 # print(a%b)
 
-# 1122
+# 1122 초를 분/초로 변환
 # a = int(input())
 # m = a//60
 # s = a%60
 # print(m,s)
 
-# 1123
+# 1123 섭씨 온도를 화씨 온도로 변환
 # a=int(input())
 # f = 9/5*a+32
 # print("%.3f" % f)
