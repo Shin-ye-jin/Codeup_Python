2f6bdc50663408b99952325ccc43fe3d56afb939
S510U <fx0605@naver.com>
2024-06-01 11:12:51 +0900

    //0601

--- 2st_week/03_01.py
+++ 2st_week/03_01.py
@@ -13,12 +13,35 @@
 #     print('big')
 
 # 1153 두 수의 대소 비교
-a,b=input().split()
-a = int(a)
-b = int(b)
-if a>b:
-    print('>')
-elif b>a:
-    print('<')
-elif a == b:
-    print('=')
+# a,b=input().split()
+# a = int(a)
+# b = int(b)
+# if a>b:
+#     print('>')
+# elif b>a:
+#     print('<')
+# elif a == b:
+#     print('=')
+
+# 1154 큰수 - 작은수
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# if a>b:
+#     print(a-b)
+# else:
+#     print(b-a)
+
+# 1155 7의 배수
+# a=int(input())
+# if a%7==0:
+#     print('multiple')
+# else:
+#     print('not multiple')
+
+# 1156 홀수 짝수 구별
+# a=int(input())
+# if a%2==0:
+#     print('even')
+# else:
+#     print('odd')
