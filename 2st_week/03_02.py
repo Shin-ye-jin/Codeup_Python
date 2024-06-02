5f6dd35dfac880a86337f3f681d6f12010e02aa0
S510U <fx0605@naver.com>
2024-06-02 11:27:03 +0900

    //0602

---
+++ 2st_week/03_02.py
@@ -0,0 +1,20 @@
+# 1157 특별한 공 던지기 1
+# a = float(input())
+# if a>=50 and a<=60:
+#     print('win')
+# else:
+#     print('lose')
+
+# 1158 특별한 공 던지기 2
+# a=int(input())
+# if (a>=30 and a<=40) or (a>=60 and a<=70):
+#     print("win")
+# else:
+#     print("lose")
+
+# 1159 특별한 공 던지기 3
+a=int(input())
+if (a>=50 and a<=70) or (a%6==0):
+    print("win")
+else:
+    print("lose")
