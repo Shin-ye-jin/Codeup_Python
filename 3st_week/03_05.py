5c53ebcf612dd19db00e5fd6429fcbe3b995d2d8
S510U <fx0605@naver.com>
2024-06-10 00:11:15 +0900

    //0607

---
+++ 3st_week/03_05.py
@@ -0,0 +1,50 @@
+# 1172 세 수 정렬하기
+# a,b,c=map(int,input().split())
+# if a<=b and a<=c:
+#     if b<=c:
+#         print(a,b,c)
+#     else:
+#         print(a,c,b)
+# elif b<=a and b<=c:
+#     if a<=c:
+#         print(b,a,c)
+#     else:
+#         print(b,c,a)
+# else:
+#     if a<=b:
+#         print(c,a,b)
+#     else:
+#         print(c,b,a)
+
+# 1173 30분전
+# a,b=map(int,input().split())
+# if b<30:
+#     if a<1:
+#         print(a+23,b+30)
+#     else:
+#         print(a-1,b+30)
+# else:
+#     print(a,b-30)
+
+# 1180 만능 휴지통
+# a=int(input())
+# c=a//10
+# d=a%10
+# e=d*10+c
+# f = e*2
+# if f<100:
+#     if f<=50:
+#         print(f)
+#         print("GOOD")
+#     else:
+#         print(f)
+#         print("OH MY GOD")
+# else:
+#     f = f-100
+#     if f<=50:
+#         print(f)
+#         print("GOOD")
+#     else:
+#         print(f)
+#         print("OH MY GOD")
+
