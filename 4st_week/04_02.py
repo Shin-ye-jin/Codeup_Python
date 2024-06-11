8d7394b594e095e62a68f49d68fa869ac89792c6
S510U <fx0605@naver.com>
2024-06-11 11:32:03 +0900

    //0611

---
+++ 4st_week/04_02.py
@@ -0,0 +1,20 @@
+# 1214 이 달은 며칠까지 있을까?
+# year,month=map(int,input().split())
+# if month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
+#     print('31')
+# elif month==2:
+#     if year%400==0 or (year%4==0 and year%100!=0):
+#         print('29')
+#     else:
+#         print('28')
+# else:
+#     print('30')
+
+# 1216 컨설팅 회사
+a,b,c=map(int,input().split())
+if a>b-c:
+    print("do not advertise")
+elif b-c>a:
+    print("advertise")
+else:
+    print("does not matter")
