638f4c193c212d8b557e67ceadecdcd246f86474
S510U <fx0605@naver.com>
2024-06-15 21:02:50 +0900

    // 0616

--- 4st_week/04_04.py
+++ 4st_week/04_04.py
@@ -36,3 +36,28 @@
 # a=int(input())
 # for i in range(1,a+1):
 #     print("*",end="")
+
+# a=int(input())
+# for i in range(0,a):
+#     print("*",end='')
+
+# 1257 두 수 사이의 홀수 출력하기
+# a,b=map(int,input().split())
+# for i in range(a,b+1):
+#     if i%2==1:
+#         print(i,end=" ")
+
+# 1258 1부터 n까지 합 구하기
+# n=int(input())
+# sum=0
+# for i in range(1,n+1):
+#     sum+=i
+# print(sum)
+
+# 1259 1부터 n까지 중 짝수의 합 구하기
+n=int(input())
+sum=0
+for i in range(1,n+1):
+    if i%2==0:
+        sum+=i
+print(sum)
