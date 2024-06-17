cc416c04f97e964616ef39b0ccd5ebffc55adf08
S510U <fx0605@naver.com>
2024-06-17 19:47:01 +0900

    // 0617

--- 5st_week/05_01.py
+++ 5st_week/05_01.py
@@ -1,1 +1,26 @@
 # 1260 3의 배수의 합
+# a,b=map(int,input().split())
+# sum=0
+# for i in range(a,b+1):
+#     if i%3==0:
+#         sum+=i
+# print(sum)
+
+# 1261 First Special Judge (Test)
+# num = map(int,input().split())
+# num=list(num) # 입력 받은 것을 배열로 변형!
+# count=0
+# for i in num:
+#     if i%5==0:
+#         print(i)
+#         count+=1
+#         break
+# if count==0:
+#     print(0)
+
+# 1265 구구단 출력하기 1
+# num=int(input())
+# for i in range(1,10):
+#     print('%d*%d=%d' % (num,i,num*i)) # %을 써서 문제 해결하기
+
+# 1266 n개의 수의 합
