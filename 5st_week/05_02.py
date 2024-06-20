a6ceb95a06d2da99d8768c99abb07ae6d66e0437
S510U <fx0605@naver.com>
2024-06-20 22:41:15 +0900

    // 0620

--- 5st_week/05_02.py
+++ 5st_week/05_02.py
@@ -21,3 +21,34 @@ for i in num:
     if i>max:
         max=i
 print(max)
+
+# 1272 기부
+a, b = map(int, input().split())
+sum = 0
+if a % 2 == 1:
+    sum += a // 2 + 1
+else:
+    sum += a * 5
+if b % 2 == 1:
+    sum += b // 2 + 1
+else:
+    sum += b * 5
+
+print(sum)
+
+# 1273 약수 구하기
+num=int(input())
+for i in range(1,num+1):
+    if num%i==0:
+        print(i,end=' ')
+
+# 1274 소수 판별
+a=int(input())
+count=0
+for i in range(2,a):
+    if a%i==0:
+        count += 1
+if count==0:
+    print("prime")
+else:
+    print("not prime")
