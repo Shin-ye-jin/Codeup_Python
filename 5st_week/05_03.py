14bd464a836cd25da90dfff9cd3a68bb146babcd
S510U <fx0605@naver.com>
2024-06-21 19:55:47 +0900

    // 0621

---
+++ 5st_week/05_03.py
@@ -0,0 +1,18 @@
+# 1275 K 제곱 구하기
+# n,k=map(int,input().split())
+# result=1
+# for i in range(0,k):
+#     result=result*n
+# print(result)
+
+# 1276 팩토리얼 계산
+# n=int(input())
+# result=1
+# for i in range(n,0,-1): # 감소
+#     result=result*i
+# print(result)
+
+# 1277 몇 번째 데이터 출력하기
+# n=int(input())
+# number=list(map(int,input().split()))
+# print(number[0],number[n//2],number[len(number)-1])
