698f947ee747331ce56b71826a7cfdad5379acda
S510U <fx0605@naver.com>
2024-06-22 14:55:42 +0900

    // 0622

--- 5st_week/05_03.py
+++ 5st_week/05_03.py
@@ -16,3 +16,33 @@
 # n=int(input())
 # number=list(map(int,input().split()))
 # print(number[0],number[n//2],number[len(number)-1])
+
+# 1278 자릿수 계산
+# n=int(input())
+# count=0
+# while n>0:
+#     n=n//10
+#     count+=1
+# print(count)
+
+# 1279 홀수는 더하고 짝수는 빼고 1
+# a,b=map(int,input().split())
+# sum=0
+# for i in range(a,b+1):
+#     if i%2==1:
+#         sum+=i
+#     else:
+#         sum-=i
+# print(sum)
+
+# 1280 홀수는 더하고 짝수는 빼고 2
+# a,b=map(int,input().split())
+# sum=0
+# for i in range(a,b+1):
+#     if i%2==1:
+#         sum+=i
+#         print('+%d' % i,end='')
+#     else:
+#         sum-=i
+#         print('-%d' %i, end='')
+# print('=%d' % sum)
---
+++ 5st_week/05_04.py
@@ -0,0 +1,18 @@
+# 1281 홀수는 더하고 짝수는 빼고 3
+# a,b=map(int,input().split())
+# sum=0
+# if a%2==1:
+#     sum+=a
+#     print(a,end='')
+# else:
+#     sum-=a
+#     print('-%d'%a,end='')
+#
+# for i in range(a+1,b+1):
+#     if i%2==1:
+#         sum+=i
+#         print('+%d'%i,end='')
+#     else:
+#         sum-=i
+#         print('-%d'%i,end='')
+# print('=%d'%sum,end='')
