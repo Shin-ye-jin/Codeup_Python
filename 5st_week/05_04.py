e0a5bc71a09e3ff8ed6daa852777f7a3ddbd45a6
S510U <fx0605@naver.com>
2024-06-23 11:13:38 +0900

    // 0623

--- 5st_week/05_04.py
+++ 5st_week/05_04.py
@@ -16,3 +16,36 @@
 #         sum-=i
 #         print('-%d'%i,end='')
 # print('=%d'%sum,end='')
+
+# 1282 제곱수 만들기
+# n=int(input())
+# i=0
+# while True:
+#     i+=1
+#     if n>=i*i and n<(i+1)*(i+1):
+#         print('%d'%(n-i*i),'%d'%i) # 출력 확인
+#         break
+
+# 1283 주식 투자
+n=int(input())
+num=int(input())
+number=list(map(int,input().split()))
+result = n
+
+for i in number:
+    if i>0:
+        n=n+n*i*0.01
+    else:
+        n=n+n*i*0.01
+result = result - n
+
+if result>0:
+    print("-%.0f"%result)
+    print('bad')
+elif result<0:
+    result=result*(-1)
+    print('%.0f'%result)
+    print('good')
+else:
+    print('%.0f'%result)
+    print('same')
