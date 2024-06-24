bf8d90a011c77558500eb93f734776208232c03b
S510U <fx0605@naver.com>
2024-06-25 00:34:18 +0900

    // 0624

--- 6st_week/06_01.py
+++ 6st_week/06_01.py
@@ -1,9 +1,73 @@
 # 1284 암호 해독
-n=int(input())
-count=0
-for i in range(2,n):
-    if n%i==0:
-        count+=1
-        print(i,end=' ')
-if count==0:
-    print('wrong number')
+# n=int(input())
+# num=[]
+#
+# for i in range(2,n):
+#     if n%i==0:
+#         num.append(i)
+#
+# if len(num)==2:
+#     if 4 in num:
+#         print("wrong number")
+#     else:
+#         print(*num)
+# else:
+#     print("wrong number")
+
+# 1285 계산기 2
+# a=input()
+# num=[]
+# count=[]
+# for i in range(len(a)):
+#     a[i].isdigit()
+#     if a[i].isdigit()==True:
+#         count.append(a[i])
+#     else:
+#         num.append(int("".join(count)))
+#         count=[]
+#         num.append(a[i])
+# cnt=1
+# result=int(num[0])
+# while True:
+#     if num[cnt]=='=':
+#         break
+#     if num[cnt]=='+':
+#         cnt+=1
+#         result+=int(num[cnt])
+#         cnt+=1
+#     elif num[cnt]=='-':
+#          cnt+=1
+#          result-=int(num[cnt])
+#          cnt+=1
+#     elif num[cnt]=='*':
+#          cnt+=1
+#          result*=int(num[cnt])
+#          cnt+=1
+#     elif num[cnt]=='/':
+#          cnt+=1
+#          result//=int(num[cnt])
+#          cnt+=1
+#
+# print(result)
+
+# 1286 최댓값, 최솟값
+n1=int(input())
+n2=int(input())
+n3=int(input())
+n4=int(input())
+n5=int(input())
+max=n1
+min=n1
+
+if max <n2: max=n2
+if max <n3: max=n3
+if max <n4: max=n4
+if max <n5: max=n5
+
+if min >n2: min=n2
+if min >n3: min=n3
+if min >n4: min=n4
+if min >n5: min=n5
+
+print(max)
+print(min)
