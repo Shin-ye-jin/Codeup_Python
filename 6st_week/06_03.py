0791096caf8a461c026f97d5c7ff6295c1f66a58
S510U <fx0605@naver.com>
2024-06-27 00:48:13 +0900

    // 0628

---
+++ 6st_week/06_03.py
@@ -0,0 +1,10 @@
+# 1357 삼각형 출력하기 4
+# n=int(input())
+# for i in range(1,n+1):
+#     for j in range(0,i):
+#         print('*',end='')
+#     print()
+# for i in range(n,1,-1):
+#     for j in range(1,i):
+#         print("*",end='')
+#     print()
