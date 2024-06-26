38f9154b013319607b6873d93b2466e20ae49026
S510U <fx0605@naver.com>
2024-06-27 00:14:43 +0900

    // 0626

---
+++ 6st_week/06_02.py
@@ -0,0 +1,19 @@
+# 1351 구구단 출력하기 2
+# a,b=map(int,input().split())
+# for i in range(a,b+1):
+#     for j in range(1,10):
+#         print("%d*%d=%d" % (i,j,i*j))
+
+# 1352 사각형 출력하기 1
+# a=int(input())
+# for i in range(0,a):
+#     for j in range(0,a):
+#         print('*',end='')
+#     print()
+
+# 1353 삼각형 출력하기 1
+# a=int(input())
+# for i in range(1,a+1):
+#     for j in range(0,i):
+#         print('*',end='')
+#     print()
