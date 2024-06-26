d75dacfdf7b3d4363bc4b6caf1fc894f3c6b05d2
S510U <fx0605@naver.com>
2024-06-27 00:35:11 +0900

    // 0627

--- 6st_week/06_02.py
+++ 6st_week/06_02.py
@@ -17,3 +17,33 @@
 #     for j in range(0,i):
 #         print('*',end='')
 #     print()
+
+# 1354 삼각형 출력하기 2
+# n=int(input())
+# for i in range(n,0,-1):
+#     for j in range(0,i):
+#         print('*',end='')
+#     print()
+
+# 1355 삼각형 출력하기 3
+# n=int(input())
+# for i in range(n,0,-1):
+#     for j in range(0,n-i):
+#         print(' ',end='')
+#     for j in range(0,i):
+#         print("*",end='')
+#     print()
+
+# 1356 사각형 출력하기 2
+# n=int(input())
+# for i in range(1,n+1):
+#     if i==1 or i==n:
+#         for j in range(0,n):
+#             print('*',end='')
+#     else:
+#         for j in range(1,n+1):
+#             if j==1 or j==n:
+#                 print("*",end='')
+#             else:
+#                 print(" ",end='')
+#     print()
