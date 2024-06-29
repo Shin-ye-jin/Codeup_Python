7137fd0aee423a8ad27322e7a116166df78efeef
S510U <fx0605@naver.com>
2024-06-29 12:26:12 +0900

    // 0628

--- 6st_week/06_03.py
+++ 6st_week/06_03.py
@@ -8,3 +8,21 @@
 #     for j in range(1,i):
 #         print("*",end='')
 #     print()
+
+# 1358 삼각형 출력하기 5
+# n=int(input())
+# for i in range(0,n//2+1):
+#     for j in range(0,n//2-i):
+#         print(' ',end='')
+#     for j in range(0,i*2+1):
+#         print('*',end='')
+#     print()
+
+# 1361 별 계단 만들기
+n=int(input())
+for i in range(0,n):
+    for j in range(0,i):
+        print(' ',end='')
+    for j in range(0,2):
+        print('*',end='')
+    print()
