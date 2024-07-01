68f3f9aeb52e4623a9073b3d82110d8e0b3deff6
S510U <fx0605@naver.com>
2024-07-01 19:13:00 +0900

    // 0701

--- 7st_week/07_01.py
+++ 7st_week/07_01.py
@@ -1,1 +1,39 @@
 # 1371 마름모 출력하기
+# n=int(input())
+# for i in range(0,n):
+#     for j in range(i,n-1):
+#         print(" ",end='')
+#     print("*",end='')
+#
+#     for j in range(0,i*2):
+#         print(" ",end='')
+#     print("*")
+#
+# for i in range(n,0,-1):
+#     for j in range(0,n-i):
+#         print(" ",end='')
+#     print("*",end='')
+#
+#     for j in range(0,(i-1)*2):
+#         print(" ",end='')
+#     print("*")
+
+# 1378 수열의 합
+# n=int(input())
+# sum=0
+# s=0
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         s = s+j
+#     sum=sum+s
+#     s=0
+# print(sum)
+
+# 1380 두 주사위의 합
+# k=int(input())
+# for i in range(1,7):
+#     for j in range(1,7):
+#         if (i+j)==k:
+#             print('%d %d'%(i,j))
+
+# 1382 GuguClass
