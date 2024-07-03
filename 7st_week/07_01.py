7a64a426806a7d98d3ea4ef28d48c25719a14038
S510U <fx0605@naver.com>
2024-07-03 18:13:45 +0900

    // 0702

--- 7st_week/07_01.py
+++ 7st_week/07_01.py
@@ -37,3 +37,39 @@
 #             print('%d %d'%(i,j))
 
 # 1382 GuguClass
+# for i in range(1,10):
+#     for j in range(2,6):
+#         print("%d x %d = %2d"%(j,i,i*j),end=' ')
+#     print()
+
+# 1677 종이 자르기
+# m,n=map(int,input().split())
+# for i in range(0,n):
+#     for j in range(0,m):
+#         if i==0 or i==n-1:
+#             if j==0 or j==m-1:
+#                 print("+",end='')
+#             else:
+#                 print("-",end='')
+#         else:
+#             if j==0 or j==m-1:
+#                 print("|",end='')
+#             else:
+#                 print(" ",end='')
+#     print()
+
+# 3122 마름모 출력하기 2
+n=int(input())
+for i in range(1,n*2+1,2):
+    for j in range(i,n*2-1,2):
+        print(" ",end='')
+    for j in range(0,i):
+        print("*",end='')
+    print()
+
+for i in range(1,n):
+    for j in range(0,i):
+        print(" ",end='')
+    for j in range(n*2-1,i*2,-1):
+        print("*",end='')
+    print()
