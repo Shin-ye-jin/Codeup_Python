e780916367c58652e55bcfaf119b9ee9e3855cfb
S510U <fx0605@naver.com>
2024-07-09 20:01:17 +0900

    // 0708

---
+++ 8st_week/08_01.py
@@ -0,0 +1,29 @@
+# 1408 암호 처리
+n=input()
+for i in n:
+  print(chr(ord(i)+2),end='')
+print()
+for i in n:
+  print(chr(ord(i)*7%80+48),end='')
+
+# 1414 C언어를 찾아라
+n=input()
+count=0
+count2=0
+for i in n:
+  if i=='c' or i=='C':
+    count+=1
+for i in range(len(n)):
+  if n[i:i+2]=='cc' or n[i:i+2]=='CC' or n[i:i+2]=='Cc' or n[i:i+2]=='cC':
+    count2+=1
+print(count)
+print(count2)
+
+# 1418 t를 찾아라
+n=input()
+count=0
+for i in n:
+  count+=1
+  if i=='t':
+    print(count,end=' ')
+
