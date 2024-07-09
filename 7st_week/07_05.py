59bee9f322fe4377c470c3d41a0bb848da1f9e81
S510U <fx0605@naver.com>
2024-07-09 19:58:38 +0900

    // 0707

--- 7st_week/07_05.py
+++ 7st_week/07_05.py
@@ -1,1 +1,24 @@
 # 1295 알파벳 대소문자 변환
+n=input()
+for i in n:
+  if ord(i)>=65 and ord(i)<=90:
+    print(chr(ord(i)+32),end='')
+  elif ord(i)>=97 and ord(i)<=122:
+    print(chr(ord(i)-32),end='')
+  else:
+    print(i,end='')
+
+# 1406 love
+n=input()
+if n=='love':
+  print('I',n,"you.")
+else:
+  print()
+
+# 1407 문자열 출력하기 1
+temp=list(input().split())
+for i in temp:
+    if i==' ':
+        continue
+    else:
+        print(i,end='')
