d539a5d80c7f48c06a32869fccfbaa263b21dc78
S510U <fx0605@naver.com>
2024-07-18 21:27:05 +0900

    // 0718

--- 9st_week/09_02.py
+++ 9st_week/09_02.py
@@ -48,3 +48,54 @@ for i in range(0,n):
   for j in range(m-1,-1,-1):
     print(matrix[i][j],end=' ')
   print()
+
+# 1479 [기초-배열연습] 2차원 배열 빗금 채우기 3-4
+n,m=map(int,input().split())
+matrix=[[0]*m for i in range(n)]
+count=0
+
+for i in range(0,n+m-1):
+  for j in range(0,m):
+    for k in range(0,n):
+      if j+k==i:
+        count+=1
+        matrix[k][j]=count
+
+for i in range(0,n):
+  for j in range(m-1,-1,-1):
+    print(matrix[i][j],end=' ')
+  print()
+
+# 1480 [기초-배열연습] 2차원 배열 빗금 채우기 3-5
+n,m=map(int,input().split())
+matrix=[[0]*m for i in range(n)]
+count=0
+
+for i in range(0,n+m-1):
+  for j in range(0,m):
+    for k in range(0,n):
+      if j+k==i:
+        count+=1
+        matrix[k][j]=count
+
+for i in range(n-1,-1,-1):
+  for j in range(m-1,-1,-1):
+    print(matrix[i][j],end=' ')
+  print()
+
+# 1481 [기초-배열연습] 2차원 배열 빗금 채우기 3-6
+n,m=map(int,input().split())
+matrix=[[0]*m for i in range(n)]
+count=0
+
+for i in range(0,n+m-1):
+  for j in range(0,n):
+    for k in range(0,m):
+      if j+k==i:
+        count+=1
+        matrix[j][k]=count
+
+for i in range(n-1,-1,-1):
+  for j in range(m-1,-1,-1):
+    print(matrix[i][j],end=' ')
+  print()
