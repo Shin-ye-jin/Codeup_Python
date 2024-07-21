e4fc34bf31fd54322a5d7ebba54830cef7859c03
S510U <fx0605@naver.com>
2024-07-21 11:09:07 +0900

    // 문제집 설정

--- 입출력문 및 연산자/01_01.py
+++ 입출력문 및 연산자/01_01.py
@@ -1,16 +1,3 @@
-# #1101 Hello, Wolrd!
-# print("Hello, World!")
-#
-# #1102 Hello, World! (줄 바꿈 버전)
-# print("Hello,\nWorld!")
-#
-# #1103 폴더명 출력
-# print("\"c:\\test\"")
-#
-# #1731 특수 서식 문자 출력하기
-# print("special charaters")
-# print("[\\n,\\\",\\\\] is very important.")
-#
 # #1110 정수 그대로 출력하기
 # a=input()
 # print(a)
---
+++ 출력문/01_01.py
@@ -0,0 +1,12 @@
+# #1101 Hello, Wolrd!
+# print("Hello, World!")
+#
+# #1102 Hello, World! (줄 바꿈 버전)
+# print("Hello,\nWorld!")
+#
+# #1103 폴더명 출력
+# print("\"c:\\test\"")
+#
+# #1731 특수 서식 문자 출력하기
+# print("special charaters")
+# print("[\\n,\\\",\\\\] is very important.")
