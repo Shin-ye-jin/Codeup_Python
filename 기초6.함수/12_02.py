97eb75b70db8419ec56075f3619736720f44821c
S510U <fx0605@naver.com>
2024-08-06 13:44:42 +0900

    // 0808

---
+++ 기초6.함수/12_02.py
@@ -0,0 +1,26 @@
+# 1538 [기초-함수작성] 함수로 odd 또는 even 출력하기
+# def f(num):
+#     if num%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+# n = int(input())
+# f(n)
+
+# 1539 [기초-함수작성] 함수로 false 또는 true 출력하기
+# def f(num):
+#     if num==0:
+#         print("false")
+#     else:
+#         print("true")
+# n=int(input())
+# f(n)
+
+# 1540 [기초-함수작성] 함수로 zero 또는 non zero 출력하기
+# def f(num):
+#     if num==0:
+#         print("zero")
+#     else:
+#         print("non zero")
+# n=int(input())
+# f(n)
