8b09f460aa887c7ea91d239d3fb238c272ec5b23
S510U <fx0605@naver.com>
2024-08-15 00:21:50 +0900

    // 0815

--- 기초6.함수/12_05(1556-1561).py
+++ 기초6.함수/12_05(1556-1561).py
@@ -18,10 +18,34 @@
 # print(f(n))
 
 # 1558 [기초-함수작성] 함수로 정수 뒤집어 리턴하기
-def f(num):
-    while num!=0:
-        number =num%10
-        print(number,end='')
-        num=num//10
-n=int(input())
-f(n)
+# def f(num):
+#     while num!=0:
+#         number =num%10
+#         print(number,end='')
+#         num=num//10
+# n=int(input())
+# f(n)
+
+# 1559 [기초-함수작성] 함수로 두 정수의 합 리턴하기
+# def f(n,m):
+#     return n+m
+# n,m=map(int,input().split())
+# print(f(n,m))
+
+# 1560 [기초-함수작성] 함수로 두 정수의 차이 값 리턴하기
+# def f(n,m):
+#     if n<m:
+#         return m-n
+#     else:
+#         return n-m
+# n,m=map(int,input().split())
+# print(f(n,m))
+
+# 1561 [기초-함수작성] 함수로 두 정수의 큰 값 리턴하기
+# def f(n,m):
+#     if n<m:
+#         return m
+#     else:
+#         return n
+# n,m=map(int,input().split())
+# print(f(n,m))
