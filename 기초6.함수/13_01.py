b87183859b11b3308163063e2fdf20a935d4272f
S510U <fx0605@naver.com>
2024-08-21 19:34:24 +0900

    // 0821

--- 기초6.함수/13_01.py
+++ 기초6.함수/13_01.py
@@ -1,14 +1,24 @@
 # 1571 [기초-함수작성] 함수로 Upper Bound 위치 리턴하기
-def f(n,k):
-    for i in range(n):
-        if num[i]>k:
-            return i+1
-    return n+1
-
-n=int(input())
-num=list(map(int,input().split()))
-k=int(input())
-
-print(f(n,k))
+# def f(n,k):
+#     for i in range(n):
+#         if num[i]>k:
+#             return i+1
+#     return n+1
+#
+# n=int(input())
+# num=list(map(int,input().split()))
+# k=int(input())
+#
+# print(f(n,k))
 
 # 1576, 1577, 1578, 1579, 1580, 1581 c전용 문제
+
+# 1602 절대값 함수
+def abs(num):
+    if num<0:
+        return (-num)
+    else:
+        return num
+
+n=float(input())
+print("%.10g"%abs(n))
