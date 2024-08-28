bc13ca73795f5829073ca84ed3b09bbb4e0c0fdf
S510U <fx0605@naver.com>
2024-08-28 20:54:33 +0900

    // 0828

--- 재귀함수/14_02.py
+++ 재귀함수/14_02.py
@@ -24,15 +24,16 @@
 # f(n)
 
 # 1929 (재귀함수) 우박수 (3n+1) (reverse)
-def f(n):
-    if n==1:
-        return 1
-    if n%2==1:
-        f(3 * n + 1)
-        print(3*n+1)
-    elif n%2==0:
-        f(n//2)
-        print(n//2)
-n=int(input())
-f(n)
-print(n)
+# def f(n):
+#     if n==1:
+#         return 1
+#     if n%2==1:
+#         f(3 * n + 1)
+#         print(3*n+1)
+#     elif n%2==0:
+#         f(n//2)
+#         print(n//2)
+#
+# n=int(input())
+# f(n)
+# print(n)
