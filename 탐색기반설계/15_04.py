0adb3fbecf0a3755940614dc68effd92b83b7fae
S510U <fx0605@naver.com>
2024-09-11 20:46:58 +0900

    // 0907

---
+++ 탐색기반설계/15_04.py
@@ -0,0 +1,16 @@
+# 3506 블럭 채우기 1 (Small)
+# def f(r,len):
+#     global cnt
+#     if r>len: return
+#
+#     if r==len:
+#         cnt+=1
+#         return
+#     else:
+#         f(r+1,len)
+#         f(r+2,len)
+#
+# n=int(input())
+# cnt=0
+# f(0,n)
+# print(cnt)
