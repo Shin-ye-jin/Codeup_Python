ea5279ac9646da3a49ec422dce5ed7f64bf8ca00
S510U <fx0605@naver.com>
2024-09-19 22:09:55 +0900

    // 0915

--- 관계기반 설계/16_05(블럭 채우기 3-8).py
+++ 관계기반 설계/16_05(블럭 채우기 3-8).py
@@ -34,19 +34,85 @@
 # print(f(n))
 
 # 3716 블럭 채우기 5
-import sys
-sys.setrecursionlimit(100000)
-num=[0]*10001
+# import sys
+# sys.setrecursionlimit(100000)
+# num=[0]*10001
+#
+# def f(n):
+#     if n==1: return 1
+#     elif n==2: return 2
+#     elif n==3: return 6
+#     elif num[n]==0:
+#         num[n]=(f(n-2)+f(n-1)+f(n-3)*3)%1000
+#         return num[n]
+#     else:
+#         return num[n]
+#
+# n=int(input())
+# print(f(n))
 
-def f(n):
-    if n==1: return 1
-    elif n==2: return 2
-    elif n==3: return 6
-    elif num[n]==0:
-        num[n]=(f(n-2)+f(n-1)+f(n-3)*3)%1000
-        return num[n]
-    else:
-        return num[n]
+# 3719 블럭 채우기 6
+# import sys
+# sys.setrecursionlimit(100000)
+# num = [0]*10001
+#
+# def f(n):
+#     if n==1: return 2
+#     elif n==2: return 7
+#     elif n==3: return 22
+#     elif num[n]==0:
+#         num[n] = (f(n-1)*3 + f(n-2)-f(n-3))%100007
+#         return num[n]
+#     else:
+#         return num[n]
+#
+# n=int(input())
+# print(f(n))
 
-n=int(input())
-print(f(n))
+# 3721 블럭 채우기 7
+# import sys
+# sys.setrecursionlimit(100000)
+# num=[0]*10001
+# num[2] = 3
+# def f(n):
+#     if num[n] !=0: return num[n]
+#
+#     if n%2==1:
+#         num[n]=0
+#         return num[n]
+#
+#     sum=0
+#
+#     for i in range(2,n-1,2):
+#         if i==2: sum+=f(n-i)*3
+#         else: sum+=f(n-i)*2
+#
+#     num[n]=(sum+2)%100007
+#     return num[n]
+#
+# n=int(input())
+# print(f(n))
+
+# 3728 블럭 채우기 8 - 시간초과
+# import sys
+# sys.setrecursionlimit(100000)
+# num=[0]*10001
+# num[1]=1
+# num[2]=5
+# def f(n):
+#     if num[n]!=0: return num[n]
+#
+#     sum=0
+#
+#     for i in range(1,n):
+#         if i==1: sum+=f(n-i)
+#         elif i==2: sum+=f(n-i)*4
+#         else:
+#             if i%2==0: sum+=f(n-i)*3
+#             else: sum+=f(n-i)*2
+#
+#     num[n]= (sum+3) % 100007 if n%2==0 else (sum+2)%100007
+#     return num[n]
+#
+# n=int(input())
+# print(f(n))
