b9fa0b4454e14bbaeca90f0f564654cb1650df2e
S510U <fx0605@naver.com>
2024-09-27 20:23:40 +0900

    // 0925

---
+++ 정올 1번 수준 문제/18_02(4741,46,51,71,76,4891).py
@@ -0,0 +1,57 @@
+# 4741 오븐 시계
+# a,b=map(int,input().split())
+# c=int(input())
+#
+# b+=c
+#
+# if b>=60:
+#     n=b//60
+#     b=b%60
+#     a+=n
+#     if a>=24:
+#         a-=24
+#         print(a,b)
+#     else:
+#         print(a,b)
+# else:
+#     print(a,b)
+
+# 4746 인공지능 시계
+# a,b,c=map(int,input().split())
+# d=int(input())
+#
+# n1=d//60
+# n2=d%60
+#
+# b+=n1
+# c+=n2
+#
+# if c>=60:
+#     n4=c//60
+#     c%=60
+#     b+=n4
+#     if b>=60:
+#         n3=b//60
+#         b%=60
+#         a+=n3
+#         if a>=24:
+#             a%=24
+#             print(a,b,c)
+#         else:
+#             print(a,b,c)
+#     else:
+#         print(a,b,c)
+# else:
+#     if b>=60:
+#         n3=b//60
+#         b%=60
+#         a+=n3
+#         if a>=24:
+#             a%=24
+#             print(a,b,c)
+#         else:
+#             print(a,b,c)
+#     else:
+#         print(a,b,c)
+
+# 4751 아시아 정보올림피아드
