cdc204c06ae1d4a2708704d47c2b8356306b1454
S510U <fx0605@naver.com>
2024-10-28 21:48:00 +0900

    // 1013

--- SFPC 2021/20_05(2323-25).py
+++ SFPC 2021/20_05(2323-25).py
@@ -2,7 +2,17 @@
 
 
 # 2324 해녀 비버 1
-
-
+# matrix1 = [12,8,5]
+# n=int(input())
+# matrix2 = list(map(int,input().split()))
+# res=0
+# res = n//matrix1[0]
+# matrix2[0] *=res
+#
+# for i in range(1,3):
+#     res = matrix2[i-1]//matrix1[i]
+#     matrix2[i]*=res
+#
+# print(matrix2[2])
 
 # 2325 덧셈왕 비버0 (스페셜 저지)
