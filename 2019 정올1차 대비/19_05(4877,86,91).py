5439ca3677c9a78e31ebcaa16409134352efa081
S510U <fx0605@naver.com>
2024-10-28 20:53:42 +0900

    // 1004

---
+++ 2019 정올1차 대비/19_05(4877,86,91).py
@@ -0,0 +1,28 @@
+# 4877 방 배정하기
+# a,b,c,n=map(int,input().split())
+# n1,n2,n3 = n,n,n
+#
+# n1%=c
+# n2%=c
+# n3%=c
+# n1%=b
+# n2%=b
+# n3%=b
+# n1%=a
+# n2%=a
+# n3%=a
+#
+# if n1==0 and n2==0 and n3==0:
+#     print(1)
+# else:
+#     print(1)
+
+# 4886 물통
+
+
+# 4891 행복
+# n=int(input())
+# matrix = list(map(int,input().split()))
+# matrix = sorted(matrix)
+# m = len(matrix)
+# print(matrix[m-1] - matrix[0])
