1331c7bb11ebbffe70733ce42e62c66e13b75e0e
S510U <fx0605@naver.com>
2024-10-28 21:57:07 +0900

    // 1014

--- SFPC 2021/21_01(2326-31).py
+++ SFPC 2021/21_01(2326-31).py
@@ -1,9 +1,40 @@
 # 2326 덧셈왕 비버 1
-
-
+# n = int(input())
+# matrix = list(map(int,input().split()))
+# a,b=map(int,input().split())
+# cnt,sum,res=0,0,0
+#
+# for i in range(a-1,b):
+#     cnt+=matrix[i]
+#
+# for i in range(n-1):
+#     for j in range(i,n):
+#         sum+=matrix[j]
+#         if sum==cnt:
+#             res+=1
+#             continue
+#     sum=0
+#
+# print(res)
 
 # 2327 덧셈왕 비버 2
-
+# n = int(input())
+# matrix = list(map(int,input().split()))
+# a,b=map(int,input().split())
+# cnt,sum,res=0,0,0
+#
+# for i in range(a-1,b):
+#     cnt+=matrix[i]
+#
+# for i in range(n-1):
+#     for j in range(i,n):
+#         sum+=matrix[j]
+#         if sum==cnt:
+#             res+=1
+#             continue
+#     sum=0
+#
+# print(res)
 
 
 # 2328 칭찬 스티커 0 (스페셜 저지)
