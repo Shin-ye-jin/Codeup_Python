6a027bde91264bb3ef0237998bfb2269dd8e2be3
S510U <fx0605@naver.com>
2024-10-28 20:57:21 +0900

    // 1006

---
+++ 그리디/19_06(2001,3120,3301).py
@@ -0,0 +1,27 @@
+# 2001 최소 대금
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=int(input())
+# e=int(input())
+#
+# m1 = min(a,b,c)
+# m2 = min(d,e)
+#
+# print(m1+m2+(m1+m2)*0.1)
+
+# 3120 리모컨
+
+
+# 3301 거스름돈
+# num = [50000,10000,5000,1000,500,100,50,10]
+# n=int(input())
+#
+# cn=0
+#
+# for i in range(8):
+#   cn = cn + n//num[i]
+#   n%=num[i]
+#
+# print(cn)
+
