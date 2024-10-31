98ed1cf422a76660a116bacc8353caebcf875364
S510U <fx0605@naver.com>
2024-10-31 22:06:10 +0900

    // 1020

--- SFPC 2022/21_03(2338-46).py
+++ SFPC 2022/21_03(2338-46).py
@@ -7,25 +7,55 @@
 
 
 # 2340 독도 새우잡이 1
-
-
-
+# n=int(input())
+# m=int(input())
+# cnt = n-m
+# matrix = list(map(int,input().split()))
+# matrix.sort()
+# matrix.reverse()
+# res=0
+#
+# for i in range(3):
+#     res+=cnt//matrix[i]
+#     cnt%=matrix[i]
+# if cnt==0:
+#     print(res)
+# else:
+#     print(-1)
 
 # 2341 특산물 교환 0 (스페셜 저지)
 
 
 
 # 2342 특산물 교환 1
-
+# n=int(input())
+# m=int(input())
+# res = n-m
+# cnt=0
+# matrix = [500,100,50,10]
+#
+# for i in range(4):
+#     cnt+=res//matrix[i]
+#     res%=matrix[i]
+#
+# print(cnt)
 
 # 2343 DNA 전사 0 (스페셜 저지)
 
 
 
 # 2344 DNA 전사 1
-
-
-
+# matrix1 = ['A','T','C','G']
+# matrix2 = ['U','A','G','C']
+#
+# n=int(input())
+# str = input()
+# num = list(str)
+#
+# for i in range(n):
+#     for j in range(4):
+#         if num[i]==matrix1[j]:
+#             print(matrix2[j],end='')
 
 # 2345 시침과 분침 0 (스페셜 저지)
 
