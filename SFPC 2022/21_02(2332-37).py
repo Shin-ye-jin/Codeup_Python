b39afe39d63e16e7485d6246c7009f8b53798618
S510U <fx0605@naver.com>
2024-10-31 21:26:52 +0900

    // 1017

--- SFPC 2022/21_02(2332-37).py
+++ SFPC 2022/21_02(2332-37).py
@@ -3,8 +3,13 @@
 
 
 # 2333 경상북도 시군 이름 1
-
-
+# a=input()
+# b=input()
+#
+# if (b in a) == True: # 문자열의 부분일치
+#     print("O")
+# else:
+#     print("X")
 
 # 2334 좋아하는 보석 0 (스페셜 저지)
 
@@ -19,3 +24,20 @@
 
 
 # 2337 청송 황금 사과 1
+from datetime import datetime, timedelta
+k=int(input())
+n,m=map(int,input().split())
+num = k-(m+n)
+cnt,x,y=0,0,0
+year,month,day = 2023,1,1
+while True:
+  cnt = 2*x+3*y
+  if cnt>=num: break
+  x+=2
+  y+=3
+res = max(x,y)
+if num==0: res+=2
+else: res+=1
+time1 = datetime(year,month,day)
+time2 = time1+timedelta(days=res)
+print(time2.strftime('%Y'),time2.strftime('%m'),time2.strftime('%d'),sep="/")
