031b664854445dcdecd4a2b47c8b9d93c0248f2c
S510U <fx0605@naver.com>
2024-10-31 22:31:07 +0900

    // 1021

--- SFPC 2022/22_01(2347-53).py
+++ SFPC 2022/22_01(2347-53).py
@@ -3,12 +3,29 @@
 
 
 # 2348 영덕대게와 울진대게 1
-
-
-
+# n = int(input())
+# matrix = [ list(map(int,input().split())) for _ in range(n)]
+# cnt = []
+# num=0
+#
+# for i in range(n-1):
+#     for j in range(n-1):
+#         num+=matrix[i][j]+matrix[i][j+1]+matrix[i+1][j]+matrix[i+1][j+1]
+#         cnt.append(num)
+#         num = 0
+# print(max(cnt))
 
 # 2349 영덕대게와 울진대게 2
-
+# n,m= map(int,input().split())
+# matrix = [ list(map(int,input().split())) for _ in range(n)]
+# cnt = []
+# num,i,j=0
+#
+# for i in range(n-1):
+#     for j in range(n-1):
+#         for j in range(m):
+#             num+=matrix[i][j]
+# print(max(cnt))
 
 
 
